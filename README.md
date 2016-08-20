f3be8227b3e859aa3ff417e7e8bfbafe94fcbd18
Wim <wim@sitebase.be>
2016-08-20 17:05:47 +0200

    working lambda function through endpoint

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ aws lambda create-function --region eu-central-1 --function-name LambdaFunctionO
 
 ## Resources
 * [Using AWS Lambda with Amazon API Gateway](http://docs.aws.amazon.com/lambda/latest/dg/with-on-demand-https.html)
+* [API Gateway step by step](http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-create-api-step-by-step.html)
--- test/functional/test.sh
+++ test/functional/test.sh
@@ -1,2 +1,6 @@
+# via console
+# aws lambda invoke --invocation-type RequestResponse --function-name LambdaFunctionOverHttps --region eu-central-1 --payload file:///Users/wim/Code/bubobox/slack-pomodoro-bot/test/functional/payloads/echo.txt outputfile.txt
 
-aws lambda invoke --invocation-type RequestResponse --function-name LambdaFunctionOverHttps --region eu-central-1 --payload file:///Users/wim/Code/bubobox/slack-pomodoro-bot/test/functional/payloads/echo.txt outputfile.txt
+# via endpoint
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+curl -X POST -d @$DIR/payloads/echo.txt "https://fburhm5u0j.execute-api.eu-central-1.amazonaws.com/test/test"
