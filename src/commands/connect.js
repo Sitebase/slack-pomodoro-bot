d7bbf38eb94b9dd9e5bcdc7a7bb48cb13b7e1cbd
Gertt <gert@gert.gert>
2016-08-20 18:36:07 +0200

    [commands] use real commands

--- src/commands/connect.js
+++ src/commands/connect.js
@@ -1,5 +1,23 @@
 module.exports = function(command, data) {
     return {
-        answer: 'Please sign in to use Pomodoro'
+        text: 'You\'re not connected to Pomodoro bot yet.',
+        attachments: [
+            {
+                text: 'Please sign in',
+                fallback: 'https://pomodoro.ambassify.com/oauth/authorize',
+                callback_id: 'connect',
+                color: '#3AA3E3',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        name: 'connect',
+                        text: 'Sign in',
+                        type: 'button',
+                        value: 'connect',
+                        style: 'primary'
+                    }
+                ]
+            }
+        ]
     };
 }
