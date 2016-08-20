6fe24424466699eff170de7296ef7fc894ce5f66
Jorgen Evens <jorgen@evens.eu>
2016-08-21 00:02:12 +0200

    [Messages] Handle mentions of people doing pomodoro

--- src/slackPomodoroBotMessages.js
+++ src/slackPomodoroBotMessages.js
@@ -1,7 +1,38 @@
-var info = require('./package.json');
+const Message = require('lib/message');
+const env = require('env.json');
+
+const mention = (data) => {
+    const names = [];
+
+    data.words.forEach(function(w) {
+        if (/^@/.test(w))
+            names.push(w);
+    });
+
+    if (names.length < 1)
+        return;
+
+    return db.list(team)
+        .then(all => {
+            return _.filter(all, v => {
+                return v.busy && names.indexOf(v.user_name) > -1
+            });
+        })
+        .then(busy => {
+            if (busy.length < 1) return;
+
+            return {
+                text: _.map(busy, v => '@' + v.user_name).join(', ') + ' are currenty doing a pomodoro';
+            };
+        });
+};
+
+const processOptions = { token: env.SLACK_VERIFICATION_TOKEN || 'gIkuvaNzQIHg97ATvDxqgjtO' };
+const messageHandler = Message(mention);
 
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        description: 'This is the messages endpoint'
-    });
+    Promise.resolve()
+        .then(() => Message.process(event))
+        .then(data => messagHandler.exec(data))
+        .catch(err => callback(err));
 };
