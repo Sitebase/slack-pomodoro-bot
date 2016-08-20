c5126c0467c9dfbfbd48d34b071063f7762bb650
Wim <wim@sitebase.be>
2016-08-20 18:22:57 +0200

    add message and commands function and deploy all functions to lambda

--- src/slackPomodoroBotMessages.js
+++ src/slackPomodoroBotMessages.js
@@ -2,8 +2,6 @@ var info = require('./package.json');
 
 exports.handler = function(event, context, callback) {
     callback(null, {
-        name: info.name,
-        version: info.version,
-        description: info.description
+        description: 'This is the messages endpoint'
     });
 };
