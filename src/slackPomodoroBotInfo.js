5f98360aa4676724ef6426b9cfae04dcd9b05f4e
Wim <wim@sitebase.be>
2016-08-20 19:33:30 +0200

    add authorize url

--- src/slackPomodoroBotInfo.js
+++ src/slackPomodoroBotInfo.js
@@ -1,9 +1,22 @@
+const qs = require('querystring');
 const info = require('./package.json');
 
+const CLIENT_ID = process.env.CLIENT_ID || 'not-found';
+const CLIENT_SECRET = process.env.CLIENT_SECRET || 'not-found';
+
 exports.handler = function(event, context, callback) {
+
+    const query = qs.stringify({
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        scope: 'identity.basic',
+        team: event.team || 'no-team'
+    });
+
     callback(null, {
         name: info.name,
         version: info.version,
-        description: info.description
+        description: info.description,
+        authorize: `https://slack.com/oauth/authorize?${query}`
     });
 };
