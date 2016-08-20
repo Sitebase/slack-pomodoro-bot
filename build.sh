8c2c1d0cf76e83f1d680b64108ef350ab9b39d42
Wim <wim@sitebase.be>
2016-08-20 17:27:26 +0200

    clean up lambda function

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 # clean
-rm dist.zip
+rm -f dist.zip
+rm -f src/package.json
 
 # build
+cp package.json src
 cd src
 zip -r ../dist.zip *
 
 # deploy
-aws lambda create-function --region eu-central-1 --function-name LambdaFunctionOverHttps --zip-file fileb:///Users/wim/Code/bubobox/slack-pomodoro-bot/dist.zip --role arn:aws:iam::244654009431:role/service-role/slack-pomodoro-bot --handler LambdaFunctionOverHttps.handler --runtime nodejs4.3
+# aws lambda create-function --region eu-central-1 --function-name LambdaFunctionOverHttps --zip-file fileb:///Users/wim/Code/bubobox/slack-pomodoro-bot/dist.zip --role arn:aws:iam::244654009431:role/service-role/slack-pomodoro-bot --handler LambdaFunctionOverHttps.handler --runtime nodejs4.3
+
+aws lambda update-function-code --region eu-central-1 --function-name LambdaFunctionOverHttps --zip-file fileb:///Users/wim/Code/bubobox/slack-pomodoro-bot/dist.zip
